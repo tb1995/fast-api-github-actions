ce342c06bd0081070d26e7f35e503508d886402f
tb1995 <tb1995@gmail.com>
2023-07-27 19:16:51 +0500

    adding more logs

--- fastapi/insert-users.sh
+++ fastapi/insert-users.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 echo "Inserting users"
 
 response=$(curl -X POST -H "Content-Type: application/json" -d '{"name": "John Doe"}'  -w "%{http_code}" -o /dev/null http://localhost:9000/users)
@@ -8,6 +10,9 @@ echo "Response code: $response"
 if [ "$response" -eq 200 ]; then
     echo "User created successfully"
 else
+    docker logs -f fastapi
+    echo "------------------------------------------------"
+    docker logs -f nginx
     echo "Failed to create user"
     exit 1
 fi
