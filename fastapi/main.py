eb293631186eb084f4fcf048566b34906824e0b6
tb1995 <tb1995@gmail.com>
2023-07-27 20:52:39 +0500

    adding CORS to see if that makes it fail

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,8 +2,18 @@ from typing import Union
 from config import db_conn, db_engine
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origins,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class User(BaseModel):
